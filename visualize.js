a9e339d132f20deeff7be8d0784b1de5aa559f17
Rishi Masand <masandrishi@gmail.com>
2019-04-26 09:43:23 -0500

    added notes to visualize

---
+++ visualize.js
@@ -0,0 +1,7 @@
+/* 
+*  Reference http://waf.cs.illinois.edu/discovery/every_gen_ed_at_uiuc_by_gpa/
+*  Make something similar to above linked GPA visualization.
+*  Use D3.js for visualization.
+*  Ability to select/change axes.
+*  Show RATING vs. PRICE, RATING vs. CUISINE, CUISINE vs. PRICE etc.
+*/
