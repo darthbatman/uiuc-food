b6c715a10699325f83dc6d1107652b2b6116d7be
Rishi Masand <masandrishi@gmail.com>
2019-05-25 20:43:52 -0700

    linted and licensed

--- src/visualize.js
+++ src/visualize.js
@@ -1,4 +1,4 @@
-/* 
+/*
 *  Reference: http://waf.cs.illinois.edu/discovery/every_gen_ed_at_uiuc_by_gpa/
 *  Dataset: ../res/dataset.json
 *  Make something similar to above linked GPA visualization.
@@ -12,123 +12,185 @@ const height = 800;
 const padding = 80;
 const dataFile = '../res/dataset.json';
 
+const lowerBoundX = 2.4;
+const upperBoundX = 5.0;
+const lowerBoundY = 0.0;
+const upperBoundY = 1.5;
+
 const mainQuadCoordinate = {
   latitude: 40.107544,
-  longitude: -88.227240
+  longitude: -88.22724,
 };
 
-var svg = d3.select('#chart')
-               .append('svg')
-               .attr('width', width)
-               .attr('height', height);
+const svg = d3.select('#chart')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
-var x = d3.scale.linear().domain([2.4, 5]).range([padding, width - padding]);
-var y = d3.scale.linear().domain([1.5, 0]).range([padding, height - padding]);
+const x = d3.scaleLinear().domain([lowerBoundX, upperBoundX]).range([padding, width - padding]);
+const y = d3.scaleLinear().domain([lowerBoundY, upperBoundY]).range([height - padding, padding]);
 
-var xAxis = d3.svg.axis().scale(x).orient('bottom');
-var yAxis = d3.svg.axis().scale(y).orient('left');
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y);
 
-var rad = function(x) {
-  return x * Math.PI / 180;
-};
-// Adapted from: https://stackoverflow.com/questions/1502590/calculate-distance-between-two-points-in-google-maps-v3
-var getDistance = function(p1, p2) {
-  var R = 6378137; // Earth’s mean radius in meter
-  var dLat = rad(p2.latitude - p1.latitude);
-  var dLong = rad(p2.longitude - p1.longitude);
-  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(rad(p1.latitude)) * Math.cos(rad(p2.latitude)) *
-    Math.sin(dLong / 2) * Math.sin(dLong / 2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c;
-  return d; // returns the distance in meter
-};
-
-var x = function(d) {
-  return (((d.rating - 2.4) / 2.6) * width + padding);
+/**
+ * Converts a degree value to radians.
+ *
+ * @param {number} degrees - The degree value
+ * @return {number} The radian value
+ */
+function radians(degrees) {
+  return degrees * Math.PI / 180;
 }
 
-var y = function(d) {
-  return (((1.5 - distanceFromCampusCenter(d.locations[0].coordinate)) / 1.5) * height - padding);
+/**
+ * Calculates distance in meters (m) between two map coordinates.
+ * Adapted from: https://stackoverflow.com/questions/1502590/calculate-distance-between-two-points-in-google-maps-v3
+ *
+ * @param {object} c1 - The first map coordinate
+ * @param {object} c2 - The second map coordinate
+ * @return {number} The distance in meters between c1 and c2
+ */
+function distanceBetween(c1, c2) {
+  const earthMeanRadius = 6378137;
+  const dLat = radians(c2.latitude - c1.latitude);
+  const dLong = radians(c2.longitude - c1.longitude);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+    + Math.cos(radians(c1.latitude)) * Math.cos(radians(c2.latitude))
+    * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return earthMeanRadius * c;
 }
 
-function distanceFromCampusCenter(coordinate) {
-    return getDistance(coordinate, mainQuadCoordinate) * 0.000621371;
+/**
+ * Calculates distance in miles (mi) between a coordinate and UIUC Main Quad.
+ *
+ * @param {object} c - The coordinate
+ * @return {number} The distance in miles between c and UIUC Main Quad
+ */
+function distanceFromMainQuad(c) {
+  const milesPerMeter = 0.000621371;
+  return distanceBetween(c, mainQuadCoordinate) * milesPerMeter;
+}
+
+/**
+ * Calculates the x position of the eatery on the chart.
+ *
+ * @param {object} eatery - The eatery
+ * @return {number} The x position
+ */
+function xPosition(eatery) {
+  return (((eatery.rating - 2.4) / 2.6) * width + padding);
+}
+
+/**
+ * Calculates the y position of the eatery on the chart.
+ *
+ * @param {object} eatery - The eatery
+ * @return {number} The x position
+ */
+function yPosition(eatery) {
+  return (((1.5 - distanceFromMainQuad(eatery.locations[0].coordinate)) / 1.5) * height - padding);
+}
+
+/**
+ * Gets the color of the eatery on the chart.
+ *
+ * @param {object} eatery - The eatery
+ * @return {string} The color as an rgb string
+ */
+function color(eatery) {
+  const distFromOrigin = Math.sqrt((xPosition(eatery) ** 2)
+    + ((height - yPosition(eatery)) ** 2));
+  if (distFromOrigin >= 900) return 'rgb(255, 75, 66)';
+  const r = 244 + (255 - 244) * (distFromOrigin / 900);
+  const g = 152 - (152 - 75) * (distFromOrigin / 900);
+  const b = 66;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+/**
+ * Gets the populated tooltip of the eatery on the chart.
+ *
+ * @param {object} eatery - The eatery
+ * @return {string} The populated tooltip as HTML
+ */
+function tooltip(eatery) {
+  return `<div style="text-align: center;">${eatery.name}</div>
+          <div class="row" style="text-align: center; margin-top: 5px; padding-top: 5px; margin-bottom: 5px; padding-bottom: 5px; border-top: dotted 1px black; border-bottom: dotted 1px black;">
+            <div class="col-xs-6">
+            <span style="font-size: 14px;">Price</span><br>
+              <span style="font-size: 28px;">$${eatery.price}</span>
+            </div>
+            <div class="col-xs-6">
+              <span style="font-size: 14px;">Distance from Main Quad</span><br>
+              <span style="font-size: 28px;">${Number.parseFloat(distanceFromMainQuad(eatery.locations[0].coordinate)).toPrecision(2)} mi</span>
+            </div>
+            <div class="col-xs-6">
+              <span style="font-size: 14px;">Rating</span><br>
+              <span style="font-size: 28px;">${eatery.rating}/5</span>
+            </div>
+          </div>`;
 }
 
 svg.append('g')
-    .attr('class', 'axis')
-    .attr('transform', 'translate(0, ' + (width - padding) + ')')
-    .call(xAxis);
- 
+  .attr('class', 'axis')
+  .attr('transform', `translate(0, ${(width - padding)})`)
+  .call(xAxis);
+
 svg.append('g')
-    .attr('class', 'axis')
-    .attr('transform', 'translate(' + padding + ', 0)')
-    .call(yAxis);
+  .attr('class', 'axis')
+  .attr('transform', `translate(${padding}, 0)`)
+  .call(yAxis);
 
-d3.json(dataFile, function (data) {
-    var r = d3.scale.linear()
-           .domain([0, d3.max(data, function (d) { return d.price; })])
-           .range([0, 8]);
+d3.json(dataFile)
+  .then((data) => {
+    const r = d3.scaleLinear()
+      .domain([0, d3.max(data, eatery => eatery.price)])
+      .range([0, 8]);
 
-    var elem = svg.selectAll("g")
-        .data(data)
+    svg.append('text')
+      .attr('class', 'label')
+      .attr('id', 'title')
+      .attr('text-anchor', 'end')
+      .attr('dx', 440)
+      .attr('y', 40)
+      .text('UIUC Food');
 
-    svg.append("text")
-      .attr("class", "x label")
-      .attr("text-anchor", "end")
-      .attr("dx", 400)
-      .attr("y", height - 4)
-      .text("Rating");
+    svg.append('text')
+      .attr('class', 'label')
+      .attr('text-anchor', 'end')
+      .attr('dx', 400)
+      .attr('y', height - 4)
+      .text('Rating');
 
-    svg.append("text")
-      .attr("class", "y label")
-      .attr("text-anchor", "end")
-      .attr("dx", -300)
-      .attr("dy", 12)
-      .attr("transform", "rotate(-90)")
-      .text("Distance from Main Quad (mi)");
+    svg.append('text')
+      .attr('class', 'label')
+      .attr('text-anchor', 'end')
+      .attr('dx', -300)
+      .attr('dy', 12)
+      .attr('transform', 'rotate(-90)')
+      .text('Distance from Main Quad (mi)');
 
-    var elemEnter = elem.enter()
-        .append("g")
-        .attr("transform", function(d){return "translate(" + x(d) + "," + y(d) + ")"})
+    const tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .html(eatery => tooltip(eatery));
+    svg.call(tip);
 
-    var tip = d3.tip()
-           .attr('class', 'd3-tip')
-           .html(function(d) {
-
-             return "<div>" + d.name + "</div>" +
-                    '<div class="row" style="text-align: center; margin-top: 5px; padding-top: 5px; margin-bottom: 5px; padding-bottom: 5px; border-top: dotted 1px black; border-bottom: dotted 1px black;">' +
-                    '<div class="col-xs-6">' +
-                    '<span style="font-size: 14px;">' + "Price" + '</span><br>' +
-                       '<span style="font-size: 28px;">$' + d.price + '</span>' +
-                    '</div>' +
-                    '<div class="col-xs-6">' +
-                      '<span style="font-size: 14px;">' + "Distance from Main Quad" + '</span><br>' +
-                      '<span style="font-size: 28px;">' + Number.parseFloat(distanceFromCampusCenter(d.locations[0].coordinate)).toPrecision(2) + ' mi</span>' +
-                    '</div>' +
-                    '<div class="col-xs-6">' +
-                      '<span style="font-size: 14px;">' + "Rating" + '</span><br>' +
-                      '<span style="font-size: 28px;">' + d.rating + '/5</span>' +
-                    '</div>' +
-                    '</div>'
-
-             return d.name + ": " + d.title + "<br>" +
-                    "Avg. GPA: " + 1 + "<br>" +
-                    "A and A+ Percentage: " + 1+"%" + "<br>" +
-                    "Gened: "+ d.GenedRequirement;
-                    /*
-             */
-           });
-           svg.call(tip);
-
-    elemEnter.append("circle")
-        .attr("r", function(d){ return d.price != 0 ? r(1000 / d.price) : 0; } )
-        .attr("class", "circle")
-        .on('mouseover', function(d){
-            tip.show(d);
-        })
-        .on('mouseout', function(d,i){
-          tip.hide(d,i);
-        });
-});
+    svg.selectAll('g')
+      .data(data)
+      .enter()
+      .append('g')
+      .attr('transform', eatery => `translate(${xPosition(eatery)},${yPosition(eatery)})`)
+      .append('circle')
+      .attr('r', eatery => (eatery.price !== 0 ? r(1000 / eatery.price) : 0))
+      .attr('fill', eatery => color(eatery))
+      .attr('class', 'circle')
+      .on('mouseover', (eatery) => {
+        tip.direction('e');
+        tip.show(eatery);
+      })
+      .on('mouseout', (eatery, i) => {
+        tip.hide(eatery, i);
+      });
+  });
